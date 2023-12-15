5b510a9c236f013dba8427c7712b775e21f59105
UnCor3 <contactuncor3@gmail.com>
2023-12-15 04:58:28 +0000

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,13 +3,13 @@ import { NextFunction, Request, Response } from "express";
 export default function middleware(
   _: Request,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) {
   const key = process.env.API_KEY!;
 
   if (key && key !== "API_KEY_HERE") next();
   else
     res.send(
-      'You don\'t have set an API key in .env file,the API is free: <a href="https://www.weatherapi.com/">here</a>,if you set already please rerun the npm run dev command'
+      'You don\'t have set an API key in .env file,the API is free: <a href="https://www.weatherapi.com/" target="_blank">here</a>,if you set already please rerun the npm run dev command',
     );
 }
